260ae22b69317b3dcbddfdf2e5331fbc5d80dce8
Cody Massey <cmassey@Administrators-MacBook-Pro.local>
2020-10-27 08:57:00 -0400

    removed the join() action in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "config_authorization_arn" {
   description = "The Amazon Resource Name (ARN) of the config authorization"
-  value       = join("", aws_config_aggregate_authorization.this.*.arn)
+  value       = aws_config_aggregate_authorization.this.*.arn
 }
 
 output "config_aggregator_arn" {
   description = "The Amazon Resource Name (ARN) of the config aggregator"
-  value       = join("", aws_config_configuration_aggregator.this.*.arn)
+  value       = aws_config_configuration_aggregator.this.*.arn
 }
 
